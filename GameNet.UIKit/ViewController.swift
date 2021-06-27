e59757d85a7e68333624f70f1a712e6769cd8210
Alliston <alliston@outlook.com>
2021-06-27 12:30:27 -0300

    Initial Commit

---
+++ GameNet.UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameNet.UIKit
+//
+//  Created by Alliston Aleixo on 27/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
